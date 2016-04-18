74e7d8d3f90bd3d096795fbc9912dbbf892d114d
groostav-desktop <geoff_groos@msn.com>
2016-04-18 00:58:10 -0700

    well this is a thing

--- test/com/softwareleyline/JavassistFixture.kt
+++ test/com/softwareleyline/JavassistFixture.kt
@@ -1,11 +1,19 @@
 package com.softwareleyline
 
+import gr.gousiosg.javacg.stat.ClassVisitor
+import gr.gousiosg.javacg.stat.MethodVisitor
 import javassist.ClassPool
 import javassist.bytecode.analysis.ControlFlow
 import javassist.bytecode.stackmap.BasicBlock
+import org.apache.bcel.Repository
+import org.apache.bcel.classfile.ClassParser
+import org.apache.bcel.classfile.JavaClass
+import org.apache.bcel.classfile.Method
+import org.apache.bcel.generic.*
 import org.assertj.core.api.Assertions.*
 import org.junit.Test
 import java.lang.reflect.Proxy
+import java.util.*
 
 /**
  * Created by Geoff on 3/22/2016.
@@ -96,4 +104,80 @@ class JavassistFixture {
 
         //now how the bugger do i get source code.
     }
+
+
+
+
+
+    @Test fun when_using_static_analysis_to_build_identifiers_should_properly_identify(){
+
+        val methodVisitorFactory = { m : MethodGen, c : JavaClass -> object : MethodVisitor(m, c) {
+            override fun visitINVOKEINTERFACE(i: INVOKEINTERFACE?) {
+                super.visitINVOKEINTERFACE(i)
+            }
+
+            override fun visitINVOKESTATIC(i: INVOKESTATIC?) {
+                super.visitINVOKESTATIC(i)
+            }
+
+            override fun visitINVOKEVIRTUAL(i: INVOKEVIRTUAL?) {
+                super.visitINVOKEVIRTUAL(i)
+            }
+
+            override fun visitINVOKESPECIAL(i: INVOKESPECIAL?) {
+                super.visitINVOKESPECIAL(i)
+            }
+        }}
+
+        val clazz = Repository.lookupClass(ExampleDag::class.java)
+
+        val classVisitor = object : ClassVisitor(clazz){
+            override fun visitMethod(method: Method?) {
+                val mg = MethodGen(method, this.clazz.className, this.constants)
+                val visitor = methodVisitorFactory(mg, clazz);
+                visitor.start()
+            }
+        }
+
+        classVisitor.start();
+    }
+}
+
+//from http://www2.cs.sfu.ca/~wsumner/teaching/886/dynamic.pdf
+class ExampleDag(){
+
+    val random = Random();
+
+    fun exampleDag(){
+
+        var goLeft = A();
+
+        if(goLeft){
+
+            var goAcross = B();
+
+            if(goAcross){
+                C();
+            }
+        }
+        else{
+            C();
+        }
+
+        val goLeftAgain = D()
+
+        if(goLeftAgain){
+            E();
+        }
+
+        F();
+    }
+
+
+    fun A() : Boolean { return random.nextBoolean(); }
+    fun B() : Boolean { return random.nextBoolean(); }
+    fun C(){}
+    fun D() : Boolean { return random.nextBoolean(); }
+    fun E(){}
+    fun F(){}
 }
